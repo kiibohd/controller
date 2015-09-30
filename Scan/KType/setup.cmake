0501e1dfd0d72a779a619a621138a3037973692a
Jacob Alexander <haata@kiibohd.com>
2015-09-29 19:29:27 -0700

    Initial code for KType TKL

---
+++ Scan/KType/setup.cmake
@@ -0,0 +1,32 @@
+###| CMake Kiibohd Controller Scan Module |###
+#
+# Written by Jacob Alexander in 2015 for the Kiibohd Controller
+#
+# Released into the Public Domain
+#
+###
+
+
+###
+# Required Sub-modules
+#
+AddModule ( Scan ISSILed )
+AddModule ( Scan MatrixARM )
+AddModule ( Scan UARTConnect )
+
+
+###
+# Module C files
+#
+set ( Module_SRCS
+	scan_loop.c
+)
+
+
+###
+# Compiler Family Compatibility
+#
+set ( ModuleCompatibility
+	arm
+)
+
