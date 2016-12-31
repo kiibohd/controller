29ed61ede535e74a407406d02b4d8b9984c4eab1
Dan Elkouby <streetwalrus@codewalr.us>
2017-01-01 01:15:23 +0200

    Drop packet upon wakeup

--- Output/pjrcUSB/arm/usb_dev.h
+++ Output/pjrcUSB/arm/usb_dev.h
@@ -63,7 +63,7 @@ void usb_isr();
 void usb_tx( uint32_t endpoint, usb_packet_t *packet );
 void usb_tx_isr( uint32_t endpoint, usb_packet_t *packet );
 
-void usb_resume();
+uint8_t usb_resume();
 
 uint32_t usb_tx_byte_count( uint32_t endpoint );
 uint32_t usb_tx_packet_count( uint32_t endpoint );
