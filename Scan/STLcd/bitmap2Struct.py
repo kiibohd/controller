2d0e09f95b80b6f6a0b37302121d779076e783c6
Joshua Schmid <jschmid@suse.de>
2017-02-23 12:20:15 +0100

    Adapt bitmaptoStruct to make it imporable/usable in other python programms.
    Signed-off-by: Joshua Schmid <jschmid@suse.de>

--- Scan/STLcd/bitmap2Struct.py
+++ Scan/STLcd/bitmap2Struct.py
@@ -20,6 +20,7 @@ import sys
 
 from array import *
 from PIL import Image # Use pillow instead of PIL, it works with Python 3
+import argparse
 
 
 # Convenience class to deal with converting images to a C array
@@ -110,89 +111,114 @@ class STLcdGraphic:
 		return display
 
 
-filename = sys.argv[1]
-if filename is None:
-	print( "You must specify a bitmap filename. Try './bitmap2Struct.py ic_logo_lcd.bmp'" )
-	sys.exit( 1 )
-max_height = 32
-max_width = 128
-x_offset = 0
-y_offset = 0
-output_image = STLcdGraphic( max_height, max_width )
+class ImageToStruct(object):
+
+    def __init__(self, filename):
+        self.max_height = 32
+        self.max_width = 128
+        self.x_offset = 0
+        self.y_offset = 0
+        self.filename = filename
+        self.output_image = STLcdGraphic( self.max_height, self.max_width )
+        self.disp_test = self.prep()
+
+    def create_and_convert_image(self):
+        # Load the input filename and convert to black & white
+        try:
+            input_image = Image.open( self.filename ).convert('1')
+        except:
+            StandardError( "Unable to load image '{0}'".format( self.filename ) )
+        return input_image
+
+    def check_boundries(self, input_image):
+        # Check the image size to see if within the bounds of the display
+        if input_image.size[0] > self.max_width or input_image.size[1] > self.max_height:
+            raise StandardError( "ERROR: '{0}:{1}' is too large, must be no larger than {2}x{3}".format(
+                filename,
+                ( input_image.format, input_image.size, input_image.mode ),
+                self.max_width,
+                self.max_height )
+                )
+
+    def center_image(self, input_image):
+        # Center the image
+        height_start = int( ( self.max_height - input_image.size[1] ) / 2 )
+        height_end   = int( height_start + input_image.size[1] )
+        width_start  = int( ( self.max_width - input_image.size[0] ) / 2 )
+        width_end    = int( width_start + input_image.size[0] )
+        return height_start, height_end, width_start, width_end
+
+    def prepare_view(self, input_image, height_start, height_end, width_start, width_end):
+        # Iterate over all of the pixels
+        # Also prepare the debug view of the image (disp_test)
+        disp_test = "+"
+        for pixel in range( 0, self.max_width ):
+                disp_test += "-"
+        disp_test += "+\n"
+
+        for y in range( 0, self.max_height ):
+                disp_test += "|"
+
+                # Check if within height range
+                if not ( y >= height_start and y < height_end ):
+                        disp_test += " " * self.max_width + "|\n|"
+                        continue
+
+                for x in range( 0, self.max_width ):
+                        # Check if within width range
+                        if not ( x >= width_start and x < width_end ):
+                                disp_test += " "
+                                continue
+
+                        # Use image value to determine pixel
+                        try:
+                                if input_image.getpixel( (x - width_start, y - height_start) ) == 0:
+                                        disp_test += "*"
+                                        self.output_image.setpixel( x, y + 1 ) # +1 is due to page boundary
+                                else:
+                                        disp_test += " "
+                        except IndexError:
+                                print( (x - width_start,y - height_start) )
+                                pass
+
+                disp_test += "|\n"
+
+        disp_test += "+"
+        for pixel in range( 0, self.max_width ):
+                disp_test += "-"
+        disp_test += "+\n"
+        return disp_test
+
+    def prep(self):
+        image = self.create_and_convert_image()
+        self.check_boundries(image)
+        height_start, height_end, width_start, width_end = self.center_image(image)
+        disp_test = self.prepare_view(image, 
+                                      height_start, 
+                                      height_end, 
+                                      width_start, 
+                                      width_end)
+        return disp_test
+
+    def output_image_fn(self): 
+        print(self.output_image.preview())
+
+    def preview(self):
+        print(self.disp_test)
+
+    def getarray(self):
+        return self.output_image.getarray()
 
 
-
-# Load the input filename and convert to black & white
-try:
-	input_image = Image.open( filename ).convert('1')
-except:
-	print( "Unable to load image '{0}'".format( filename ) )
-
-# Check the image size to see if within the bounds of the display
-if input_image.size[0] > max_width or input_image.size[1] > max_height:
-	print( "ERROR: '{0}:{1}' is too large, must be no larger than {2}x{3}".format(
-		filename,
-		( input_image.format, input_image.size, input_image.mode ),
-		max_width,
-		max_height )
-	)
-	sys.exit( 1 )
-
-# Center the image
-height_start = int( ( max_height - input_image.size[1] ) / 2 )
-height_end   = int( height_start + input_image.size[1] )
-width_start  = int( ( max_width - input_image.size[0] ) / 2 )
-width_end    = int( width_start + input_image.size[0] )
-
-#print( height_start )
-#print( height_end )
-#print( width_start )
-#print( width_end )
-
-# Iterate over all of the pixels
-# Also prepare the debug view of the image (disp_test)
-disp_test = "+"
-for pixel in range( 0, max_width ):
-	disp_test += "-"
-disp_test += "+\n"
-
-for y in range( 0, max_height ):
-	disp_test += "|"
-
-	# Check if within height range
-	if not ( y >= height_start and y < height_end ):
-		disp_test += " " * max_width + "|\n|"
-		continue
-
-	for x in range( 0, max_width ):
-		# Check if within width range
-		if not ( x >= width_start and x < width_end ):
-			disp_test += " "
-			continue
-
-		# Use image value to determine pixel
-		try:
-			if input_image.getpixel( (x - width_start, y - height_start) ) == 0:
-				disp_test += "*"
-				output_image.setpixel( x, y + 1 ) # +1 is due to page boundary
-			else:
-				disp_test += " "
-		except IndexError:
-			print( (x - width_start,y - height_start) )
-			pass
-
-	disp_test += "|\n"
-
-disp_test += "+"
-for pixel in range( 0, max_width ):
-	disp_test += "-"
-disp_test += "+\n"
-
-# BMP Conversion preview
-print( disp_test )
-
-
-print ( output_image.preview() )
-#print ()
-print( "uint8_t array[] = {0};".format( output_image.getarray() ) )
-
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--filename", 
+                        type=str, 
+                        required=True,
+                        help="provide .bmp file")
+    args = parser.parse_args()
+    filename = args.filename
+    prep = ImageToStruct(filename)
+    prep.preview()
+    prep.output_image_fn()
+    print("uint8_t array[] = {}".format(prep.getarray()))
