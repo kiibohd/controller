fc62987465236eac418e86b42f7070c3e22d506b
Jacob Alexander <haata@kiibohd.com>
2017-05-03 16:45:11 -0700

    Deprecating non-maintained Scan Modules
    - Will likely still work
    - Deprecating to show that they are no longer maintained
      * Un-deprecating is an option if there is a need

---
+++ Scan/Deprecated/MBC1000/setup.cmake
@@ -0,0 +1,32 @@
+###| CMake Kiibohd Controller Scan Module |###
+#
+# Written by Jacob Alexander in 2014 for the Kiibohd Controller
+#
+# Released into the Public Domain
+#
+# For Sanyo MBC-1000 keyboards
+#
+###
+
+
+###
+# Module C files
+#
+
+set( SCAN_SRCS
+	scan_loop.c
+)
+
+
+###
+# Module Specific Options
+#
+
+
+###
+# Compiler Family Compatibility
+#
+set( ScanModuleCompatibility
+	arm
+)
+
