fda219810325dd261d62faf5abc61de2a1fba3fc
Jacob Alexander <haata@kiibohd.com>
2017-08-30 19:53:26 -0700

    More missing build scripts

---
+++ Bootloader/Builds/whitefox.bash
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# This is a build script template
+# These build scripts are just a convenience for configuring your keyboard (less daunting than CMake)
+# Jacob Alexander 2017
+
+
+
+#################
+# Configuration #
+#################
+
+# Should be empty if not set
+BOOT_PRODUCT_STR=WhiteFox
+
+
+
+########################
+# Bash Library Include #
+########################
+
+# Shouldn't need to touch this section
+
+# Check if the library can be found
+if [ ! -f mk20dx256vlh7.bash ]; then
+	echo "ERROR: Cannot find 'mk20dx256vlh7.bash'"
+	exit 1
+fi
+
+# Load the library
+source mk20dx256vlh7.bash
+
