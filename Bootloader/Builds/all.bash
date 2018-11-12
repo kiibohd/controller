e57aeb72991c6a7bb57094a48066741b7f95e7d5
Jacob Alexander <haata@kiibohd.com>
2018-11-11 22:40:17 -0800

    More travis fixes

--- Bootloader/Builds/all.bash
+++ Bootloader/Builds/all.bash
@@ -33,12 +33,12 @@ cmd_cpy ./mk22fx512avlh12.bash kiibohd_bootloader.bin bootloader/mk22fx512avlh12
 cmd_cpy ./sam4s8b.bash         kiibohd_bootloader.bin bootloader/sam4s8b.bootloader.bin
 cmd_cpy ./sam4s8c.bash         kiibohd_bootloader.bin bootloader/sam4s8c.bootloader.bin
 cmd_cpy ./ergodox.bash         kiibohd_bootloader.bin bootloader/ergodox.bootloader.bin
+cmd_cpy ./geminiduskdawn.bash  kiibohd_bootloader.bin bootloader/geminiduskdawn.bootloader.bin
 cmd_cpy ./infinity.bash        kiibohd_bootloader.bin bootloader/infinity.bootloader.bin
 cmd_cpy ./infinity_led.bash    kiibohd_bootloader.bin bootloader/infinity_led.bootloader.bin
 cmd_cpy ./kira.bash            kiibohd_bootloader.bin bootloader/kira.bootloader.bin
 cmd_cpy ./ktype.bash           kiibohd_bootloader.bin bootloader/ktype.bootloader.bin
 cmd_cpy ./whitefox.bash        kiibohd_bootloader.bin bootloader/whitefox.bootloader.bin
-cmd_cpy ./xtype.bash           kiibohd_bootloader.bin bootloader/xtype.bootloader.bin
 
 # Tally results
 result
